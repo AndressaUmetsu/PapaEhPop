5fcc8edf24241f8c17069b511f9e51ad33fb7d36
AndressaUmetsu <andressa.umetsu@gmail.com>
2016-03-13 18:11:18 -0300

    lista

--- exercicios.hs
+++ exercicios.hs
@@ -1,19 +1,14 @@
--- exe 1
 inverter[] = []
 inverter(x:xs) = inverter xs ++ [x]
 
-{- exe 2 -}
 pali s = if inverter s == s then True else False
 
--- exe 3
 pertence a [] = False
 pertence a (x:xs) = if a == x then True else pertence a xs
 
-{- exe 4 -}
 inter [] _ = []
 inter (x:xs) ys = if pertence x ys then x:inter xs ys else inter xs ys
-
--- exe 5 
+ 
 repete [] = False
 repete (x:xs) = if pertence x xs then True else repete xs
 
@@ -25,8 +20,10 @@ ultimos n xs = take n (inverter xs)
 expo b 0 = 1
 expo b e = b * expo b (e-1) 
 
---binToint
---binToint (x:xs)
+converteBin [] _ = 0
+converteBin (x:xs) e = if x == '1' then expo 2 e + converteBin xs (e+1) else converteBin xs (e+1)
+
+binToint xs = converteBin (inverter(xs)) 0 
 
 menor [x] = x 
 menor (x:xs) = min x (menor xs)

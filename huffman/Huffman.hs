1ce4cbbcf54f2b3f4ee21bd3df937aee535885a3
zuchis <liquimaster@gmail.com>
2016-05-11 17:02:53 -0300

    made the leaf list creator function

--- huffman/Huffman.hs
+++ huffman/Huffman.hs
@@ -2,7 +2,18 @@ module Huffman where
 
 import Data.List
 
-data Huffman = Folha Char Int | No Int Huffman Huffman
+data Huffman = Folha Char Int | No Int Huffman Huffman deriving (Show, Eq)
+
+instance Ord Huffman where
+    compare (Folha _ x) (Folha _ y)
+        | x > y = GT
+        | x < y = LT
+        | otherwise = EQ
+
+    compare (Folha _ x) (No y _ _)
+        | x > y = GT
+        | x < y = LT
+        | otherwise = EQ 
 
 compareTuples a b
     | snd a < snd b = LT
@@ -22,7 +33,15 @@ removeDups (x:xs)
     | otherwise     = x : removeDups xs
 
 --frequenceList [] = []
-frequenceList xs = zip characters (listFrequenceCalculator characters xs)
+frequenceList xs = sortBy compareTuples $ zip characters (listFrequenceCalculator characters xs)
     where
         characters = removeDups xs 
 
+huffmanList_ [] = []
+huffmanList_ (tuple:tupleList) = Folha (fst tuple) (snd tuple) : huffmanList_ tupleList
+
+huffmanList xs = huffmanList_ tupleList
+    where
+        tupleList = frequenceList xs
+
+--huffmanTree (leaf:leafList) 

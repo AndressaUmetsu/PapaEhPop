6ad3d1845d933a56d422f3280196c24a76cd9457
Zuchis <liquimaster@gmail.com>
2016-05-13 20:48:47 -0300

    I don't know what to do anymore

--- huffman/expand.hs
+++ huffman/expand.hs
@@ -5,22 +5,31 @@ import System.IO
 import System.IO.Error
 
 
+canMakeChar :: [Char] -> Huffman -> Bool
 canMakeChar _ (Folha _ _) = True
-canMakeChar [] _ =  False
+canMakeChar [] (No _ _ _) =  False
 canMakeChar (x:xs) (No _ left right)
     | x == '0' = canMakeChar xs right
     | x == '1' = canMakeChar xs left
-    
-makeChar _ (Folha a _) = a
+
+makeChar :: [Char] -> Huffman -> [Char]
+makeChar _ (Folha a _) = [a]
 makeChar (x:xs) (No _ left right)
     | x == '0' = makeChar xs right
     | x == '1' = makeChar xs left
 
 
-expand [] _ = []
-expand xs tree = makeChar charCode tree : expand (drop (length charCode) xs) tree
-    where 
-        charCode = takeWhile (not . \x -> canMakeChar x tree) xs 
+expand_ :: [Char] -> [Char] -> Huffman -> [Char]
+expand_  _ [] _ = ""
+expand_ [] _ _ = ""
+expand_ charCode (x:xs) tree
+    | canMakeChar charCode tree = makeChar charCode tree ++ expand_ (head restOfTheList) (tail restOfTheList) tree
+    | otherwise = expand_ (charCode ++ [x]) xs tree
+    where
+        restOfTheList = drop (length charCode) xs
+
+expand :: [Char] -> Huffman -> [Char]
+expand (x:xs) tree = expand_ [x] xs tree
 
 
 main = do

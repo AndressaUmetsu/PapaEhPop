79584a3ce82c0e2901d4554c89c411aac304d253
Zuchis <liquimaster@gmail.com>
2016-05-28 16:08:44 -0300

    finished expansion function

--- huffman/expand.hs
+++ huffman/expand.hs
@@ -17,10 +17,17 @@ getRegs n = do
     if n == 0 then return[]
         else do {r <- getReg; rs <- getRegs (n-1); return(r:rs);}
 
+getEncoded n
+    | n == 0 = ""
+    | n > 7 = int2bin $ ord $ I.w2c $ G.getWord8 ++ getEncoded (n-8)
+    | otherwise = take n $ int2bin $ ord $ I.w2c $ G.getWord8 
+
 getAll = do
     numOfCharacters <- G.getWord32be
     wordLength <- G.getWord32be
     let freqList = getRegs numOfCharacters
+    let encodedWord = getEncoded wordLength
+    return((freqList,encodedWord))
 
 expand tree = expand' tree
     where
@@ -28,7 +35,17 @@ expand tree = expand' tree
         expand' (Folha a _) xs = a : expand' tree xs
         expand' (No _ left right) (x:xs) = expand' (if x == '0' then right else left) xs
 
+int2bin x
+    | x `div` 2 == 0 = show x
+    | otherwise = int2bin (x `div` 2) ++ show (x `mod` 2) 
+
 main = do
     args <- getArgs
-    file <- readFile (head args)
+    file <- L.readFile (head args)
+    let 
+        (freqList,encodedWord) = G.runGet getAll file
+        leafList = huffmanList_ freqList
+        tree = huffmanTree_ leafList
+        expandedWord = expand (head tree) encodedWord
+    writeFile "output.out" expandedWord
     return ()

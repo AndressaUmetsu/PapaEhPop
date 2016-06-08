afb73897da973582f9f677f67f1ff54b9560a514
zuchis <liquimaster@gmail.com>
2016-06-08 19:47:01 -0300

    parser in progress

--- parser/logicalParser.hs
+++ parser/logicalParser.hs
@@ -1,66 +1,85 @@
 import Text.ParserCombinators.Parsec
 import Data.Char
 
-data Expr = Bimp Expr Expr | Imp Expr Expr | Or Expr Expr | And Expr Expr | Not Expr Expr | Var String deriving Show
+data Expr = Bimp Expr Expr | Imp Expr Expr | Or Expr Expr | And Expr Expr | Not Expr | Var String deriving Show
 
 main = do
     putStr "\nExpressao:"
-    e <- getLine;
+    e <- getLine
     case avaliarExpr e of
         Left err -> putStr ((show err)++ "\n")
-        Right r  -> putStr ((show r) ++ "\n")}
-    return ()
+        Right r  -> putStr ((show r) ++ "\n")
+    return()
+
 
 avaliarExpr e = parse expr "Erro:" e
 
 ret v1 Nothing = v1
 ret v1 (Just (op, v2)) = op v1 v2
 
+expr = do
+	v1 <- imp  -- E -> IB'
+	e <- bimp'
+	return (ret v1 e )
 
- -- E -> FE'
-expr = do v1 <- term
-		  e <- expr'
-		  return (ret v1 e )
+bimp' = do {string "<->"; -- B' -> <->IB'
+		v1 <- imp;
+		e <- bimp';
+		return (Just (Bimp, ret v1 e))}
+	 <|> return Nothing -- B' -> Vazio
 
-o = do v1 <- a -- O -> AO'
-	   e <- o'
-	   return (ret v1 e)
+imp = do
+		v1 <- _or -- I -> AI'
+		e <- _or'
+		return (ret v1 e )
 
--- Or
-o' = do {string "||"; -- O' -> ||AO'
-		 v1 <- a;
-		 e <- o';
-		 return (Just ((Or), ret v1 e))}
+imp' = do {string "->"; -- I' -> ->AI'
+		v1 <- _and;
+		e <- imp';
+		return (Just (Imp, ret v1 e))}
+	 <|> return Nothing -- expr' -> Vazio
+
+_or = do
+	v1 <- _and -- O -> AO'
+	e <- _or'
+	return (ret v1 e)
+
+_or' = do {string "||"; -- O' -> ||AO'
+		v1 <- _and;
+		e <- _or';
+		return (Just (Or, ret v1 e))}
 	 <|> return Nothing -- O' -> Vazio
 
--- And
-a = do v1 <- n-- A -> NA'
-	   e <- a'
-	   return (ret v1 e)
+_and = do
+	v1 <- _not-- A -> NA'
+	e <- _and'
+	return (ret v1 e)
 
-a' = do {string "&&"; -- A' -> &&NA'
-		 v1 <- n;
-		 e <- a';
-		 return (Just ((And), ret v1 e))}
+_and' = do {string "&&"; -- A' -> &&NA'
+		v1 <- _not;
+		e <- _and';
+		return (Just (And, ret v1 e))}
 	 <|> return Nothing -- A' -> Vazio
 
+--Not
+--_not = do { char '!'; --N -> _not N
+--		 e <- _not -- ??
+--		 return (Just((Not), ret  e))}
+--	<|>	 return	fator  -- N -> F
 
-n = do { char '!'; --N -> not N
-		 v1 <- n;
-		 e <- -- ??
-		 return (Just((Not), ret v1 e))}
-	<|>	 return	fator  -- N -> F
+_not = return fator
 
-fator = bool												-- F -> c
+fator = bool 												-- F -> c
 		<|> do { char '('; e <- expr; char ')'; return e } 	-- F -> (E)
 
-
-
+bool = do
+	literal <- many1 letter
+	return literal
 
 
 
 --avaliarTab (Not e1) = not (avali
---avaliarTab (And e1 e2) = avaliarTab e1 && avaliarTab e2
+--avaliarTab (_And e1 e2) = avaliarTab e1 && avaliarTab e2
 --avaliarTab (Var v) = consultarValor tab v
 
 --tab = [("x1",True),("x2",False)]
